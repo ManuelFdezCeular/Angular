b9ae78b94858c6a1c26441d7899606cc5cd2d097
ManuelFdezCeular <manuel.jesus.fernandez.ceular@gmail.com>
2020-02-27 10:04:56 +0100

    commit

--- petClinic/src/app/Componentes/form-vet/form-vet.component.ts
+++ petClinic/src/app/Componentes/form-vet/form-vet.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Vet } from 'src/app/Modelos/vet';
+import { Especialidad } from 'src/app/Modelos/especialidad';
+import { VetService } from 'src/app/Servicios/vet.service';
+import { EspecialidadesService } from 'src/app/Servicios/especialidades.service';
 
 @Component({
   selector: 'app-form-vet',
@@ -6,10 +11,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form-vet.component.css']
 })
 export class FormVetComponent implements OnInit {
+  
+  public accion:string;
+  public veterinario:Vet;
+  public especialidades:Especialidad[];
+  public idVet:number = this.ruta.snapshot.params["idVeterinario"];
 
-  constructor() { }
+  constructor(private servicioVeterinario:VetService, private servicioEsp:EspecialidadesService, private router:Router, private ruta:ActivatedRoute) { 
+    this.veterinario = <Vet>{};
+    this.servicioEsp.getEspecialidades().subscribe(resultado=>{
+      this.especialidades = resultado;
+    })
+    if(this.idVet == -1){
+      this.accion = "Añadir";
+    }else{
+      this.accion = "Modificar";
+      this.servicioVeterinario.getVet(this.idVet).subscribe(resultado=>{
+        console.log("veterinario:", resultado);
+        this.veterinario = resultado;
+      })
+    }
+  }
 
   ngOnInit() {
+    
   }
 
 }
