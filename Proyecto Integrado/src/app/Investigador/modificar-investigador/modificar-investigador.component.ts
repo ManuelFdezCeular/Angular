af210b53a93f94027b0b517b922dbf230b10ef72
UltraBinario1991 <ManuelFdezCeular>
2020-03-20 20:24:36 +0100

    commit

--- Proyecto Integrado/src/app/Investigador/modificar-investigador/modificar-investigador.component.ts
+++ Proyecto Integrado/src/app/Investigador/modificar-investigador/modificar-investigador.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Investigador } from '../investigador';
+import { ModificarInvestigadorService } from './modificar-investigador.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import { DescripcionInvestigadorService } from '../descripcion-investigador/descripcion-investigador.service';
 
 @Component({
   selector: 'app-modificar-investigador',
@@ -6,10 +10,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./modificar-investigador.component.css']
 })
 export class ModificarInvestigadorComponent implements OnInit {
+  
+  public investigador:Investigador;
+  public id:number = this.ruta.snapshot.params["id"];
 
-  constructor() { }
+
+  constructor(private servicioModificarServicio:ModificarInvestigadorService, private servicioDescripcionInvestigador:DescripcionInvestigadorService, private router:Router, private ruta:ActivatedRoute) {
+    this.servicioDescripcionInvestigador.obtenerInvestigador(this.id).subscribe(resultado=>{
+      console.log("resultado: ",resultado);
+      this.investigador = resultado;
+    })
+  }
 
   ngOnInit() {
+  }
+
+  modificarInvestigador(){
+    console.log("investigador modificado:", this.investigador)
+    this.servicioModificarServicio.modificar(this.investigador).subscribe(resultado=>{
+      this.router.navigate(["/investigadores"]);
+    })
   }
 
 }
--- Proyecto Integrado/src/app/Investigador/modificar-investigador/modificar-investigador.service.ts
+++ Proyecto Integrado/src/app/Investigador/modificar-investigador/modificar-investigador.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Investigador } from '../investigador';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ModificarInvestigadorService {
+  
+  url = "http://localhost/AJAX/ServidorProyectoIntegrado/MiServicio.php";
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  modificar(investigador:Investigador){
+    let objeto = JSON.stringify({
+      accion: "ModificarInvestigador",
+      investigador: investigador
+    })
+
+    return this.http.post<boolean>(this.url, objeto);
+  }
 }
